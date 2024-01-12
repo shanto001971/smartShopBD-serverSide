4409111bb459a704b3aaff575fb0a10f90f905b8
Shanto Islam <rajs45693@gmail.com>
2024-01-12 20:04:00 +0600

    add some missing api

--- index.js
+++ index.js
@@ -41,7 +41,8 @@ async function run() {
         const db = client.db('shop');
 
         const cardCollection = db.collection('productsCollaction');
-        const cartCollection = db.collection("cartCollection")
+        const cartCollection = db.collection("cartCollection");
+        const proceedCollection = db.collection("proceedCollection");
 
         app.get('/cardCollection', async (req, res) => {
             const result = await cardCollection.find().toArray();
@@ -49,7 +50,7 @@ async function run() {
         });
         app.get('/cardCollection/:id', async (req, res) => {
             const id = req.params.id;
-            console.log(id)
+            // console.log(id)
             const query = { _id: new ObjectId(id) };
             const result = await cardCollection.findOne(query);
             res.send(result);
@@ -74,16 +75,9 @@ async function run() {
             res.send(results)
         })
 
-        app.post("/carts", async (req, res) => {
-            const carts = req.body;
-            // console.log(carts)
-            const result = await cartCollection.insertOne(carts);
-            res.send(result)
-        });
-
         app.get('/api/search', async (req, res) => {
             try {
-                const { query, category,page = 1, pageSize = 10  } = req.query;
+                const { query, category, page = 1, pageSize = 10 } = req.query;
 
                 let filter = {};
                 if (query) {
@@ -102,6 +96,87 @@ async function run() {
                 res.status(500).json({ error: 'Internal Server Error' });
             }
         });
+
+        app.get("/confirmOrder", async (req, res) => {
+            const email = req.query.email;
+            // console.log(email)
+            if (!email) {
+                res.send([]);
+            };
+
+            const query = { email: email };
+            const results = await proceedCollection.find(query).toArray();
+            // console.log(results)
+            res.send(results)
+        })
+
+
+        app.post("/carts", async (req, res) => {
+            const carts = req.body;
+            // console.log(carts)
+            const result = await cartCollection.insertOne(carts);
+            res.send(result)
+        });
+
+        app.post("/proceedToCheckOut", async (req, res) => {
+            try {
+                const product = req.body.product;
+                const options = { ordered: true };
+                const result = await proceedCollection.insertMany(product, options);
+                res.send(result);
+            } catch (error) {
+                console.error('Error processing checkout:', error);
+                res.status(500).send('Internal Server Error');
+            }
+        });
+
+        app.post("/placeOrder", async (req, res) => {
+            const product = req.body;
+            // console.log(product)
+            const result = await proceedCollection.insertOne(product);
+            res.send(result)
+        });
+
+        app.delete("/carts", async (req, res) => {
+            const productIds = req.body.productIds;
+            // console.log(productIds)
+            const objectIds = productIds.map(id => new ObjectId(id));
+            // Specify the filter to match documents for deletion
+            const filter = { _id: { $in: objectIds } };
+            // Perform the delete operation
+            const result = await cartCollection.deleteMany(filter);
+            res.send(result)
+        });
+
+        app.delete("/cart/:id", (req, res) => {
+            const { id } = req.params;
+            const query = { _id: new ObjectId(id) };
+            const result = cartCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        app.delete("/cancelOrder/:id", async (req, res) => {
+            try {
+                const { id } = req.params;
+                // console.log(id)
+                const query = { productsId: id };
+
+                const result = await proceedCollection.deleteOne(query);
+                // console.log(result)
+
+                // res.send(result)
+
+                if (result.deletedCount === 1) {
+                    res.status(200).send({ message: "Order cancelled successfully" });
+                } else {
+                    res.status(404).send({ message: "Order not found" });
+                }
+            } catch (error) {
+                console.error("Error cancelling order:", error);
+                res.status(500).send({ error: "Internal server error" });
+            }
+        });
+
 
 
         // Send a ping to confirm a successful connection
